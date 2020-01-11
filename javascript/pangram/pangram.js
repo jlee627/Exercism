364ce194cf6758931a222fb72693e98a5044c408
jlee627 <32377243+jlee627@users.noreply.github.com>
2020-01-11 00:21:37 -0800

    pangram notes

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -6,3 +6,5 @@
 export const isPangram = (possiblePangram) => {
   return new Set(possiblePangram.toLowerCase().match(/[a-z]/g)).size === 26
 };
+
+// Notes: Items in set can only occur once which works well for this problem
